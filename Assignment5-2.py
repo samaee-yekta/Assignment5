106d1d2dd8095700f22f6a00a7f4aaef65fb7ddf
fatemeh-samaee-yekta <148364835+samaee-yekta@users.noreply.github.com>
2023-11-07 16:25:28 +0330

    Update Assignment5-2.py

--- Assignment5-2.py
+++ Assignment5-2.py
@@ -1,7 +1,12 @@
+import math
 n = int(input("Enter the lenght: "))
 m = int(input("Enter the widgh: "))
 
 for i in range(n):
-    for j in range(m):
-       print("#""*",end = "")
-    print()
+   for j in range(m):
+
+      if (i + j) %2 ==0:
+         print("#", end= "")
+      else:
+         print("*", end= "")
+   print()
